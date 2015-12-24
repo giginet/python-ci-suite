7a3df60469b052eeb6e944f668fa355ec2b7ad2f
giginet <giginet.net@gmail.com>
2015-12-25 00:35:37 +0900

    Add mock test case

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,10 +1,20 @@
 from unittest import TestCase
 from calculator.calculator import Calculator
 
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import MagicMock
+
 
 class CalcTestCase(TestCase):
     def test_add(self):
         self.assertEqual(1 + 1, 2)
+
+    def test_mock(self):
+        mock_object = MagicMock()
+        mock_object.__str__.return_value = 'hello'
+        self.assertEqual(str(mock_object), 'hello')
 
 
 class CalculatorTestCase(TestCase):
